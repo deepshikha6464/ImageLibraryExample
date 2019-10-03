70929d48a5c32d28d2263b67aeb933e027e10054
Deepshikha <deepshikha6464@gmail.com>
2019-10-03 12:27:18 +0530

    loading image with URL

--- app/src/main/java/com/example/imagelibraryexample/MainActivity.java
+++ app/src/main/java/com/example/imagelibraryexample/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.imagelibraryexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    //ui component
+     ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.image);
+        //loading image with URL
+        Glide.with(this)
+                .load("https://www.android.com/static/2016/img/share/andy-lg.png")
+                .into(imageView);
     }
 }
