6b747cb2a8690bd180cd773e09f08aae2a4f7c40
Deepshikha <deepshikha6464@gmail.com>
2019-10-03 12:35:04 +0530

    centre cropped image

--- app/src/main/java/com/example/imagelibraryexample/MainActivity.java
+++ app/src/main/java/com/example/imagelibraryexample/MainActivity.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class MainActivity extends AppCompatActivity {
 
     //ui component
-     ImageView imageView;
+     ImageView imageView, imageView2, imageView3,imageView4,imageView5, imageView6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +19,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.image);
+        imageView2 = findViewById(R.id.image2);
+        imageView3 = findViewById(R.id.image3);
+        imageView4 = findViewById(R.id.image4);
+        imageView5 = findViewById(R.id.image5);
+        imageView6 = findViewById(R.id.image6);
+
         //loading image with URL
         Glide.with(this)
                 .load("https://www.android.com/static/2016/img/share/andy-lg.png")
                 .into(imageView);
+
+        //centre cropped image ... RequestOption()
+        RequestOptions requestOptions = new RequestOptions()
+                .centerCrop();
+
+        Glide.with(this)
+                .setDefaultRequestOptions(requestOptions)
+                .load("https://www.android.com/static/2016/img/share/andy-lg.png")
+                .into(imageView2);
     }
 }
